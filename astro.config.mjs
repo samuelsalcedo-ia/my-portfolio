c61a86f14b17ff5edd5d9984499867347e123cc1
Samuel <serviciosrurales@dmrural.es>
2025-09-18 14:34:23 +0200

    Ajusta config de Astro para Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: 'https://samuelsalcedo-ia.github.io/my-portfolio/',
-  base: '/my-portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
