d2588cde79c5910e7563c188e339a685b325ecb7
Samuel <serviciosrurales@dmrural.es>
2025-09-18 14:09:26 +0200

    fix(ci): Corregir definitivamente el workflow de despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
-
 export default defineConfig({
   site: 'https://samuelsalcedo-ia.github.io/my-portfolio/',
-  base: '/my-portfolio/', 
+  base: '/my-portfolio/',
   vite: {
     plugins: [tailwindcss()],
   },
